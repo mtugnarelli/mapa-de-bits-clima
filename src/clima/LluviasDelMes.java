ddd1ea2a7a1319dcc129e6d0d107dc19ce355d52
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-06-25 02:19:26 -0300

    Contiene un día

--- src/clima/LluviasDelMes.java
+++ src/clima/LluviasDelMes.java
@@ -24,6 +24,17 @@ public class LluviasDelMes {
 		validar(dia);
 		llovio[dia - 1] = true;
 	}
+	
+	/**
+	 * @pre 'dia' está en el rango [1-31].
+	 * @param dia : número del día del mes.
+	 * @return si llovió el día del mes.
+	 */
+	public boolean contiene(int dia) {
+		
+		validar(dia);
+		return llovio[dia - 1];
+	}
 
 	private void validar(int dia) {
 		if ((dia < 1) || (dia > 31)) {
